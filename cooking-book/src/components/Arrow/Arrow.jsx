f69a24c27421b835b0b036d1e3784b25a703403a
czesuaww <greg152295@gmail.com>
2025-12-30 10:41:01 +0100

    Created header with arrow and changed style in app css and index css

---
+++ cooking-book/src/components/Arrow/Arrow.jsx
@@ -0,0 +1,18 @@
+import style from "./Arrow.module.css";
+
+const Arrow = () => {
+    const scrollDown = () => {
+        const aboutMeSection = document.getElementById("workstation");
+        if (aboutMeSection) aboutMeSection.scrollIntoView({ behavior: "smooth" });
+    };
+
+    return (
+        <div className={style.container}>
+            <div onClick={scrollDown}>
+                <div className={style.arrow}></div>
+            </div>
+        </div>
+    );
+};
+
+export default Arrow;
---
+++ cooking-book/src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import img1 from '../../images/header/header1.jpg';
+import img2 from '../../images/header/header2.jpg';
+import img3 from '../../images/header/header3.jpg';
+import styles from './Header.module.css';
+
+import Arrow from '../Arrow/Arrow';
+
+const Header = () => {
+    return (
+        <div className={styles.carousel}>
+            <div className={styles.slide}>
+                <img src={img1} alt="picture one" />
+            </div>
+            <div className={styles.slide}>
+                <img src={img2} alt="picture two" />
+            </div>
+            <div className={styles.slide}>
+                <img src={img3} alt="picture three" />
+            </div>
+            <div className={styles.heroText}>
+                <h1>Looking for some recepies?</h1>
+                <p>You've come to the perfect place.
+                    <br />
+                    I invite you to search for the best recipes
+                </p>
+                <Arrow />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
