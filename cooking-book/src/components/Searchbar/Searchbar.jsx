b2d6d22b1b36a6d9b22fa93f8a713178d42f4038
czesuaww <greg152295@gmail.com>
2026-01-14 19:23:07 +0100

    added small validation to search input

--- cooking-book/src/components/Searchbar/Searchbar.jsx
+++ cooking-book/src/components/Searchbar/Searchbar.jsx
@@ -2,20 +2,27 @@ import './Searchbar.module.css';
 import style from './Searchbar.module.css';
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { useNavigate } from 'react-router';
+import { useState } from 'react';
 
 const Searchbar = () => {
     const [term, setTerm] = useLocalStorage('query', '');
     const [, setLastSearch] = useLocalStorage('last-search', null);
     const navigate = useNavigate();
+    const [error, setError] = useState(false);
     const search = (e) => setTerm(e.target.value);
 
     const searchRecepie = () => {
+        if (term.trim() === '') {
+            setError(true);
+            return;
+        }
+        setError(false);
         navigate({ pathname: '/search', search: `?fraze=${term}` })
 
         if (term.trim()) setLastSearch(term);
     }
 
-    const enterSearch = (e) => e.key === 'Enter' ? searchRecepie(term) : null;
+    const enterSearch = (e) => e.key === 'Enter' ? searchRecepie() : null;
 
     return (
         <>
@@ -27,6 +34,7 @@ const Searchbar = () => {
                 onKeyDown={enterSearch}
                 value={term}
             />
+            {error && <h1 className={style.error}>Input cannot be empty</h1>}
             <button className={style.searchBtn} onClick={searchRecepie}>Search</button>
         </>
     )
