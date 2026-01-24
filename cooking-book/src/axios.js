84df90781ff54d3d7b72f2d3525aac610ae94412
czesuaww <greg152295@gmail.com>
2026-01-24 18:18:43 +0100

    created env

--- cooking-book/src/axios.js
+++ cooking-book/src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: 'https://cooking-react-811f4-default-rtdb.europe-west1.firebasedatabase.app' })
+const instance = axios.create({ baseURL: import.meta.env.VITE_DATABASE })
 
 export default instance
